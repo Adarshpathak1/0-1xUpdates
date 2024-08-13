00f918159cb698cf53c162439181b111a5460040
adarsh pathak <adarshpathak826@gmail.com>
2024-08-13 11:00:15 +0530

    info removed for connectioin

--- week3/3.2/dbtest.js
+++ week3/3.2/dbtest.js
@@ -1,16 +1,23 @@
+require("dontenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://Admin-Adarsh_db:rm9u1mh371u3EzZX@cluster0.eg48g.mongodb.net/userAppNew");
+mongoose
+  .connect(
+    "mongodb+srv://Admin-Adarsh_db:rm9u1mh371u3EzZX@cluster0.eg48g.mongodb.net/userAppNew",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 const User = mongoose.model("users", {
-    name: String,
-    email: String,
-    password: String
+  name: String,
+  email: String,
+  password: String,
 });
 const user = new User({
-    name: "Harkirat",
-    email: "harkirat@email.com",
-    password: "123"
+  name: "Harkirat",
+  email: "harkirat@email.com",
+  password: "123",
 });
 
 user.save();
